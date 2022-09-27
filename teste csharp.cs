cc83b918875f418f93ba8334214603f5f5865685
Andre Picolo <delpicolo.ad@gmail.com>
2022-09-26 21:43:02 -0300

    teste do csharp

---
+++ teste csharp.cs
@@ -0,0 +1,17 @@
+//operadores unários
+
+var X = false;
+var n1 = 6;
+var n2 = 5;
+var n3 = 7;
+var n4 = 8;
+
+Console.WriteLine(!X);
+n3++;
+Console.WriteLine(n3);
+n4--;
+Console.WriteLine(n4);
+Console.WriteLine(n2 == --n1)
+Console.WriteLine(n2-- == ++n1);
+
+//na execução ficaria true, 8, 7, true, falese. 
