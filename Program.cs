4ff062c60696e4a4fddc94045e02864ad92ef1e0
Andrea Del Picolo <109634520+adpicolo@users.noreply.github.com>
2022-10-03 10:56:08 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,29 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-//Ler um numero inteiro, um caractere, um numero double e 
-//ler um nome, sexo, idade e altura na mesma linha
-//armazenando-os em quatro variaveis com os devidos tipos.
+string nome = Console.ReadLine();
+int quarto = int.Parse(Console.ReadLine());
+double preçoProduto = double.Parse(Console.ReadLine());
 
-int n1 = int.Parse(Console.ReadLine());//int.Parse converte string em numeros inteiros;
-char ch = char.Parse(Console.ReadLine()); // converter string para char será char.Parse; 
-double n2 = double.Parse(Console.ReadLine()); //para converter é double.parse; Tem que digitar o numero com virgula e nãoponto ex 4,32; 
-
-
-string[] vet = Console.ReadLine().Split(' '); //para ser vetror sempre usar [] após o string.
-string nome = vet[0];
-char sexo = char.Parse(vet[1]);
-int idade = int.Parse(vet[2]);
-double altura = double.Parse(vet[3]);
+string[] vet= Console.ReadLine().Split(' ');
+string ultimoNome = vet[0];
+int idade = int.Parse(vet[1]);
+double  altura = double.Parse(vet[2]);
 
 
 
-Console.WriteLine("você digitou: ");
-Console.WriteLine(n1);
-Console.WriteLine(ch);
-Console.WriteLine(n2);
-Console.WriteLine(nome);
-Console.WriteLine(sexo);
+
+Console.WriteLine("digitwe seu nome completo: " + nome);
+Console.WriteLine("digite quantos quartos tem em sua casa: " + quarto);
+Console.WriteLine("digite o preço do produto:" + preçoProduto);
+Console.WriteLine("entre digitando seu ultimo nome, idade e altura: ");
+Console.WriteLine(ultimoNome); 
 Console.WriteLine(idade);
-Console.WriteLine(altura);
+Console.WriteLine(altura);  
+
 
 
