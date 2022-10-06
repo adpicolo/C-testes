77e675bf476822886e207ab4699bf19c4e18e114
Andrea Del Picolo <109634520+adpicolo@users.noreply.github.com>
2022-10-06 16:26:21 -0300

    Saída de dados
    Técnica Placeholders, interpolação, concatenação. Ambas significam introduzir variáveis em frases de modo mais prático.

--- Program.cs
+++ Program.cs
@@ -1,46 +1,23 @@
-﻿internal class Program
+﻿
+//IMPRIMIR SAÍDA DE DADOS Csharp
+
+
+internal class Program
 {
     private static void Main(string[] args)
     {
+        
 
-        bool completo = false; //guarda valor verdade /V ou F; 
-        char genero = 'F'; //guarda caracteres; entre aspas simples; 
-        char letra = '\u0041'; //codigo unicolde
-        byte n1 = 126;// guarda de 0 a 255; 
-        int n2 = 1000; //int numeros inteiros; 
-        int n3 = 2147483647; //numero máximo do int;
-        long n4 = 2147483648L; //após o int, usar o long; recomenda-se usar o L ao final para verificar que é numero Long; 
-        float n5 = 4.5f;//precisa colocar o f na frente para saber q é float; 
-        double n6 = 4.5; // o double suporta numeros maiores queo float; 
-        string nome = "Andrea Del Picolo"; //string são palavras; não é tipo basico da linguagem c#; é cadeia de caractere Unicode; IMUTÁVEL; 
-        //não pode mais mudar o valor da string após criada; para deixar o programa mais seguro e livre de erros; 
-        object obj1 = "Junior Amor";  //objeto genérico;
-        object obj2 = 37.0; //pode também numeros doubles geral;
+        
+        int idade = 31;
+        double saldo = 10.35784;
+        string nome = "Maria"; 
 
-        int n7 = int.MinValue; //pega o valor minimo do codigo descrito; 
-        int n8 = int.MaxValue; // valor máximo do codigo digitado;
-        sbyte n9 = sbyte.MinValue; //menor valor do byte; 
-        decimal n10 = decimal.MaxValue; 
+        Console.WriteLine("{0} tem {1} anos e tem saldo igual a {2:F} reais", nome, idade, saldo); // técnica Placeholders; iniciar contar do zero e colocar entre as chaves; 
 
+        Console.WriteLine($"{nome} tem {idade} anos e tem saldo igual a {saldo:F2} reais");//tecnica interpolação; 
 
-
-        Console.WriteLine(completo); 
-        Console.WriteLine(genero);
-        Console.WriteLine(letra);
-        Console.WriteLine(n1);
-        Console.WriteLine(n2);
-        Console.WriteLine(n3);
-        Console.WriteLine(n4);
-        Console.WriteLine(n5);
-        Console.WriteLine(n6);
-        Console.WriteLine(nome);
-        Console.WriteLine(obj1);
-        Console.WriteLine(obj2); 
-        Console.WriteLine(n7);
-        Console.WriteLine(n8);
-        Console.WriteLine(n9);
-        Console.WriteLine(n10);
-
+        Console.WriteLine(nome + " tem " +  idade + "anos e tem saldo igual a " + saldo.ToString("F2") + " reais");//concondenação de strings - junção de varias partes; 
 
     }
 }
