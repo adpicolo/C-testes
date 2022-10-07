2f0b22b4630085324b0498e806494d489e77504d
Andrea Del Picolo <109634520+adpicolo@users.noreply.github.com>
2022-10-07 17:44:39 -0300

    Demais testes iniciais
    Estruturas iniciais no C# como soma, media, estruturas de repetição if, if else e else.

--- Program.cs
+++ Program.cs
@@ -1,46 +1,99 @@
-﻿using System.Globalization;
+﻿using System.Runtime.Serialization;
+
 internal class Program
 {
     private static void Main(string[] args)
     {
-        string produto1 = "computador";
-        string produto2 = "mesa de escritorio";
+        double saldo = 100.0;
+        double valorDoSaque = 10.0;
+        saldo -= valorDoSaque; //pode-se utilizar -= para simplificar o codigo, o que seria antes: saldo = saldo - valorDoSaque;
+        //operadores += (para	somas),	*=	(para	multiplicações)	e /=	(para	divisões).
 
-        byte idade = 30;
-        int codigo = 5290;
-        char genero = 'M';
+        Console.WriteLine(valorDoSaque);
 
-        double preco1 = 2100.0;
-        double preco2 = 650.50;
-        double medida = 53.234567;
+        string mensagem = "Minha	Mensagem";
+        Console.WriteLine(mensagem);
+        Console.WriteLine("--------------------------------------------------");
 
-        Console.WriteLine("Produtos: ");
-        Console.WriteLine(produto1 + " cujo o preço é: " + preco1);
-      
-        Console.WriteLine(produto2 + " cujo preço é " + preco2);
+        int idadeMaria = 20;
+        int idadeJoao = 30;
+        int idadeAna = 40;
+        int media = 0; 
+        int Soma = 0;
 
-        Console.WriteLine("Registro: ");
-        Console.WriteLine(idade + "anos de idade, código " + codigo + " gênero " + genero);
-
-        Console.WriteLine("Medida com oito casas decimais: " + medida);
-        Console.WriteLine("arredondando (tres casas decimais): {0:F3}", medida);
-        Console.WriteLine("Separador decimal invariant culture: " + medida.ToString("F3", CultureInfo.InvariantCulture));
-
-
-        Console.WriteLine("-------------------------------------------------------");
-        Console.WriteLine("Produtos: ");
-        Console.WriteLine("{0} cujo preço é $ {1:F2} ", produto1, preco1);
-        Console.WriteLine("{0} cujo preço é $ {1:F2} ", produto2, preco2);
-        Console.WriteLine("Registro: {0} anos de idade, código {1} e gênero {2} ", idade, codigo, genero);
-        Console.WriteLine("Medida com oito casas decimais: {0:F8}", medida);
-        Console.WriteLine("Arredondando três casas decimais: {0:F3} ", medida);
-        Console.WriteLine("Separador decimal inavriant Culture: " + medida.ToString("F3", CultureInfo.InvariantCulture));
-
-    
-
-
-
-
+        Soma += idadeAna + idadeJoao + idadeMaria;
+        media = Soma / 2; 
         
+        Console.WriteLine("A média da idade é: " + media);
+        Console.WriteLine("--------------------------------------------------");
+
+
+        double pi = 3.14;
+        int piQuebrado = (int)pi;
+
+        Console.WriteLine("piQuebrado	=	" + piQuebrado);
+        Console.WriteLine("--------------------------------------------------");
+
+        double saldo2 = 100.0;
+        double valorSaque = 10.0;
+        if (saldo2 >= valorSaque)
+            //ambos são tipo bool - valores V ou F; 
+        {
+            saldo = saldo2 - valorSaque;
+
+            Console.WriteLine("saque autorizado");
+        }
+
+        else; 
+            Console.WriteLine("saldo insuficiente");
+
+
+        // > menor; < mair; == igual; != diferente; ! negar uma condição; && e; || OU;
+        // ex: bool	realmentePodeSacar	=	(saldo	>=	valorSaque)	&&	(valorSaque	>	0);
+        // ex. OU - essa	condição	é	verdadeira	se	(saldo	>=	valorSaque)	for	true
+        //	ou	se	(valorSaque	>	0)	for	verdadeiro.
+
+        Console.WriteLine("----------------------------------");
+
+        double taxa;
+
+        Console.WriteLine(" digite o valor de seu saldo: "); 
+        
+
+        if (saldo < 1000)
+        {
+            taxa = 0.01;
+        }
+        else if (saldo <= 5000)
+        {
+            taxa = 0.05;
+        }
+        else
+        {
+            taxa = 0.1; 
+        }
+
+        double saldo3 = 500.0;
+        if (saldo < 0.0)
+        {
+            Console.WriteLine("você esta no negativo");
+
+        }
+        else if (saldo < 1000000.0) ;
+        {
+            Console.WriteLine("você é um bom cliente");
+        }
+
+            
+        else
+        {
+            Console.WriteLine("você é milionário!!!!");
+        }
+
+
+
+
+
+
     }
 }
