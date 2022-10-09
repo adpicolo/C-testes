578f892cfe9f05c961ffe1d6ce464684f60fb97a
Andrea Del Picolo <109634520+adpicolo@users.noreply.github.com>
2022-10-09 20:28:25 -0300

    básico de dados de entrada

--- Program.cs
+++ Program.cs
@@ -1,95 +1,29 @@
-﻿using System.Runtime.Serialization;
+﻿using System.Threading.Channels;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
-        double saldo = 100.0;
-        double valorDoSaque = 10.0;
-        saldo -= valorDoSaque; //pode-se utilizar -= para simplificar o codigo, o que seria antes: saldo = saldo - valorDoSaque;
-        //operadores += (para	somas),	*=	(para	multiplicações)	e /=	(para	divisões).
+        string frase = Console.ReadLine();
+        //abre o terminal para receber a informação do usuário; 
+        string x = Console.ReadLine();
+        string y = Console.ReadLine();
+        string z = Console.ReadLine();
 
-        Console.WriteLine(valorDoSaque);
-
-        string mensagem = "Minha	Mensagem";
-        Console.WriteLine(mensagem);
-        Console.WriteLine("--------------------------------------------------");
-
-        int idadeMaria = 20;
-        int idadeJoao = 30;
-        int idadeAna = 40;
-        int media = 0; 
-        int Soma = 0;
-
-        Soma += idadeAna + idadeJoao + idadeMaria;
-        media = Soma / 2; 
+        //comando Split - vetor; abre espaço entre parenteses para colocar palvaras na mesma linha;
         
-        Console.WriteLine("A média da idade é: " + media);
-        Console.WriteLine("--------------------------------------------------");
+        string[] v = Console.ReadLine().Split(' ');
+        string a = v[0];
+        string b = v[1];
+        string c = v[2]; 
 
-
-        double pi = 3.14;
-        int piQuebrado = (int)pi;
-
-        Console.WriteLine("piQuebrado	=	" + piQuebrado);
-        Console.WriteLine("--------------------------------------------------");
-
-        double saldo2 = 100.0;
-        double valorSaque = 10.0;
-        if (saldo2 >= valorSaque)
-            //ambos são tipo bool - valores V ou F; 
-        {
-            saldo = saldo2 - valorSaque;
-
-            Console.WriteLine("saque autorizado");
-        }
-
-        else; 
-            Console.WriteLine("saldo insuficiente");
-
-
-        // > menor; < mair; == igual; != diferente; ! negar uma condição; && e; || OU;
-        // ex: bool	realmentePodeSacar	=	(saldo	>=	valorSaque)	&&	(valorSaque	>	0);
-        // ex. OU - essa	condição	é	verdadeira	se	(saldo	>=	valorSaque)	for	true
-        //	ou	se	(valorSaque	>	0)	for	verdadeiro.
-
-        Console.WriteLine("----------------------------------");
-
-        double taxa;
-
-        Console.WriteLine(" digite o valor de seu saldo: "); 
-        
-
-        if (saldo < 1000)
-        {
-            taxa = 0.01;
-        }
-        else if (saldo <= 5000)
-        {
-            taxa = 0.05;
-        }
-        else
-        {
-            taxa = 0.1; 
-        }
-
-        double saldo3 = 500.0;
-        if (saldo < 0.0)
-        {
-            Console.WriteLine("você esta no negativo");
-
-        }
-        else if (saldo < 1000000.0) ;
-        {
-            Console.WriteLine("você é um bom cliente");
-        }
-
-            
-        else
-        {
-            Console.WriteLine("você é milionário!!!!");
-        }
-
+        Console.WriteLine("voce digitou: " + frase);
+        Console.WriteLine(x);
+        Console.WriteLine(y);
+        Console.WriteLine(z);
+        Console.WriteLine(a);
+        Console.WriteLine(b);
+        Console.WriteLine(c);
 
 
 
