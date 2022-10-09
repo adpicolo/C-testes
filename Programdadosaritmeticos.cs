c6653dbc24d19699c8a259a8fe37edf1d83d0c93
Andrea Del Picolo <109634520+adpicolo@users.noreply.github.com>
2022-10-09 20:08:51 -0300

    Inicio de dados aritmeticos
    Dados simples de matemática; Casting;

---
+++ Programdadosaritmeticos.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int n1 = 3 + 4 * 2;
+        int n2 = (3 + 4) * 2;
+        int n3 = 17 % 3;
+        double n4 = (double) 10 / 8; 
+        //como a variavél parece ser numeros inteiros, tera que colocar o CASTING (double) 10/8; 
+
+        Console.WriteLine( n1);
+        Console.WriteLine(n2);
+        Console.WriteLine(n3);
+        Console.WriteLine(n4);
+        
+        }
+
+    }
+}
