e618205e61efeb05eee641aa9505301a8243f1ab
Andrea Del Picolo <109634520+adpicolo@users.noreply.github.com>
2022-10-11 11:40:48 -0300

    True or False
    Exemplos de operações com T, F, &&(e), ||(ou) e ! (negação);

--- Program.cs
+++ Program.cs
@@ -1,34 +1,29 @@
-﻿using System; 
-using System.Globalization;
-using System.Runtime.Serialization;
-
-internal class Program
+﻿internal class Program
 {
     private static void Main(string[] args)
     {
-        /*Fazer um programa que leia o número de um funcionário, seu número de horas trabalhadas, 
-        o valor que recebe por hora e calcula o salário desse funcionário. A seguir, mostre o número 
-        e o salário do funcionário, com duas casas decimais.*/
+        //tabela verdade -e xemplos
 
-        int numero, horas;
-        double valorHora, salario;
+        bool c1 = 2 > 3 && 4 != 5; // como 2 não é maior do q 3 e precisaria de mambos para ser verdadeira, dará falso;
+        //condição && (E) será verdadeira somente quando ambas forem V;
 
-        numero = int.Parse(Console.ReadLine());
-        horas = int.Parse(Console.ReadLine());
-        valorHora = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        Console.WriteLine(c1);
 
-        salario = valorHora * horas;
+        //na condição OU || - ao menos uma tem que ser V;
+        bool c2 = 2 > 3 || 4 != 5; // TRUE
+        Console.WriteLine(c2); //pelo menos uma das duas~é V então, o resultado sera verdadeiro; ! significa NÃO; 
 
-        Console.WriteLine("NUMBER = " + numero);
-        Console.WriteLine("SALARY = U$ " + salario.ToString("F2", CultureInfo.InvariantCulture));
-        //To.String....serve para colocar pontos ao invez de virgulas nas casas decimais; 
+        bool c3 = !(2 > 3) && 4 != 5; //! negao resultado, inverte; /TRUE por conta da negação !; 
+        Console.WriteLine(c3);
+        Console.WriteLine("------------------------------");
 
+        bool c4 = 10 < 5;
 
-
-
-
-
-
+        bool c5 = c1 || c2 && c3;
+        Console.WriteLine(c5);
+        // C2 EE C3 = Precede = V + V = V;
+        //C1 é falso + V DO c2 E c3 = V; pois OU exige que somente uma seja V; 
+        
 
 
 
