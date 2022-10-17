5a1d7ad223fc4e874fdd60964788aa48c36af56e
Andrea Del Picolo <109634520+adpicolo@users.noreply.github.com>
2022-10-17 10:23:08 -0300

    Teste exercicio do triangulo
    Exercício resolvido sem orientação a objetos.

--- Program.cs
+++ Program.cs
@@ -1,24 +1,50 @@
-﻿internal class Program
+﻿using System;
+using System.Globalization;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        //estrutura for/para
-        // for(inicio; condição; incremento){ comandos}
-        // costuma ser utilizada quando sabe a quantidade de reptições; se for ocntagem;
-        //caso contrário, utiliza while; 
+        //USO DO PROBLEMA SEM ORIENTAÇÃO A OBJETOS; 
+        /*Fazer um programa para ler as medidas dos lados de dois triângulos X e Y (suponha medidas
+        válidas). Em seguida, mostrar o valor das áreas dos dois triângulos e dizer qual dos dois triângulos
+        possui a maior área.
+        A fórmula para calcular a área de um triângulo a partir das medidas de seus lados a, b e c é a
+        seguinte (fórmula de Heron):
+        area  p( p  a)( p  b)( p  c) 2
+        a b c
+        */
 
 
-        Console.Write("quantos numeros inteiros voce vai digitar: ");
-        int n = int.Parse(Console.ReadLine());
-        int soma = 0;
+double xA, xB, xC, yA, yB, yC;
+Console.WriteLine("entre com as medidas do triangulo X: ");
+xA = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+xB = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+xC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-        for(int i =1; i <=n; i++)
-        {
-            Console.Write("valor #{0}: ", i);
-            int valor = int.Parse(Console.ReadLine());
-            soma += valor; 
-        }
+Console.WriteLine("entre com as medidas do triangulo Y: ");
+yA = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+yB = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+yC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-        Console.WriteLine("soma = " + soma);
-    }
+double p = (xA + xB + xC )/ 2.0;
+double areaX = Math.Sqrt(p * (p - xA) * (p - xB) * (p - xC));
+
+p = (yA + yB + yC) / 2.0;
+double areaY = Math.Sqrt(p * (p - yA) * (p - yB) * (p - yC));
+
+Console.WriteLine("Area de x =  " + areaX.ToString("F4", CultureInfo.InvariantCulture));
+Console.WriteLine("Area de Y =  " + areaY.ToString("F4", CultureInfo.InvariantCulture));
+
+if (areaX > areaY)
+{
+    Console.WriteLine("Maior area: X");
+
+}
+else
+{
+    Console.WriteLine("maior area: Y");
+}
+
+}
 }
