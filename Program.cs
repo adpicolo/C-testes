082722b68fe41ddb82ea2f8aa944a8e3c95e6f92
Andrea Del Picolo <109634520+adpicolo@users.noreply.github.com>
2022-10-18 11:52:42 -0300

    Introdução a classes csharp
    Primeira aula de introdução a classes e objetos.

--- Program.cs
+++ Program.cs
@@ -1,50 +1,33 @@
-﻿using System;
-using System.Globalization;
-
-internal class Program
+﻿internal class Jogador
+    //onde inicia a classe
 {
-    private static void Main(string[] args)
+    public int energia=100;
+    //se nao estiver nada escrito, será public automaticamente; 
+
+    public bool vivo=true;
+
+
+    //onde inicia os objetos da classe
+    public static void Main(string[] args)
     {
-        //USO DO PROBLEMA SEM ORIENTAÇÃO A OBJETOS; 
-        /*Fazer um programa para ler as medidas dos lados de dois triângulos X e Y (suponha medidas
-        válidas). Em seguida, mostrar o valor das áreas dos dois triângulos e dizer qual dos dois triângulos
-        possui a maior área.
-        A fórmula para calcular a área de um triângulo a partir das medidas de seus lados a, b e c é a
-        seguinte (fórmula de Heron):
-        area  p( p  a)( p  b)( p  c) 2
-        a b c
-        */
+        
+        Jogador j1 = new Jogador(); 
+        //o new reserva a memorai e retorna o endereço da memoria
+        //que ele alocou nesse objeto; 
+        Jogador j2 = new Jogador();
+        Jogador j3 = new Jogador();
+
+        j1.energia = 50;
+        j2.energia = 30; 
+        j1.vivo = false;
+        j2.vivo = true; 
+
+        Console.WriteLine("energia do jogador 1: {0}", j1.energia);
+        Console.WriteLine("energia do jogador 1: {0}", j2.energia);
+
+        Console.WriteLine("o jogador 1 esta vivo? "+ j1.vivo);
+        Console.WriteLine("o jogador 2 esta vivo?  "+ j2.vivo);
 
 
-double xA, xB, xC, yA, yB, yC;
-Console.WriteLine("entre com as medidas do triangulo X: ");
-xA = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-xB = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-xC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-
-Console.WriteLine("entre com as medidas do triangulo Y: ");
-yA = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-yB = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-yC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-
-double p = (xA + xB + xC )/ 2.0;
-double areaX = Math.Sqrt(p * (p - xA) * (p - xB) * (p - xC));
-
-p = (yA + yB + yC) / 2.0;
-double areaY = Math.Sqrt(p * (p - yA) * (p - yB) * (p - yC));
-
-Console.WriteLine("Area de x =  " + areaX.ToString("F4", CultureInfo.InvariantCulture));
-Console.WriteLine("Area de Y =  " + areaY.ToString("F4", CultureInfo.InvariantCulture));
-
-if (areaX > areaY)
-{
-    Console.WriteLine("Maior area: X");
-
-}
-else
-{
-    Console.WriteLine("maior area: Y");
-}
-
-}
+    }
 }
