95f729f6a3c69875b94012881ed726677328a707
Andrea Del Picolo <109634520+adpicolo@users.noreply.github.com>
2022-10-18 21:41:15 -0300

    introdução a classe e objeto2
    Métodos, construtores, destrutortes.

--- Program.cs
+++ Program.cs
@@ -1,33 +1,50 @@
 ﻿internal class Jogador
-    //onde inicia a classe
+//onde inicia a classe
 {
-    public int energia=100;
+    public int energia;
     //se nao estiver nada escrito, será public automaticamente; 
 
-    public bool vivo=true;
+    public bool vivo;
 
+    public string nome;//escopo global a classe;
 
-    //onde inicia os objetos da classe
-    public static void Main(string[] args)
+    public Jogador(string n)//escopo diferente; local ao metodo
     {
-        
-        Jogador j1 = new Jogador(); 
-        //o new reserva a memorai e retorna o endereço da memoria
-        //que ele alocou nesse objeto; 
-        Jogador j2 = new Jogador();
-        Jogador j3 = new Jogador();
-
-        j1.energia = 50;
-        j2.energia = 30; 
-        j1.vivo = false;
-        j2.vivo = true; 
-
-        Console.WriteLine("energia do jogador 1: {0}", j1.energia);
-        Console.WriteLine("energia do jogador 1: {0}", j2.energia);
-
-        Console.WriteLine("o jogador 1 esta vivo? "+ j1.vivo);
-        Console.WriteLine("o jogador 2 esta vivo?  "+ j2.vivo);
+        energia = 100;
+        vivo = true;
+        nome = n; 
+        //metodo Destrutor - deletar o metodo; 
 
 
     }
-}
+    
+
+        //onde inicia os objetos da classe -INSTANCIAÇÃO
+        public static void Main(string[] args)
+
+        //metodo construtor
+    {
+        string nome1;//metodo construtor; 
+        Console.WriteLine("digite o nome do jogador 1: ");
+        nome1 = Console.ReadLine(); 
+        Jogador j1 = new Jogador(nome1);
+        //o new reserva a memorai e retorna o endereço da memoria
+        //que ele alocou nesse objeto; 
+        Jogador j2 = new Jogador("THEO");
+        
+        j1.energia = 50;
+        j2.energia = 30;
+
+
+        Console.WriteLine("nome do jogador 1: {0}", j1.nome);
+        Console.WriteLine("nome do jogador 2: {0}", j2.nome);
+
+    }
+    
+    }
+    
+        
+    
+
+
+
